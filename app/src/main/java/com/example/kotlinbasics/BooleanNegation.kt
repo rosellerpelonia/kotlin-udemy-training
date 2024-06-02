c3adf2f2d807f41f7cfe482d8e4e22f4f958b46b
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 15:22:05 +0800

    Boolean negation

---
+++ app/src/main/java/com/example/kotlinbasics/BooleanNegation.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasics
+
+fun main(){
+    val myTrue: Boolean = true
+    val myFalse: Boolean = false
+
+    println(myTrue || myFalse)
+    println(myTrue && myFalse)
+    println(!myTrue)
+}
