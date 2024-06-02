7c1043f8d1f835d6f466c34284d4ad505226c715
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 15:50:38 +0800

    IfStatements

---
+++ app/src/main/java/com/example/kotlinbasics/IfStatements.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    val age = 35
+
+    if(age >= 18){
+        println("You can buy alcohol")
+    }else if(age <=18){
+        println("You cannot buy alcohol")
+    }else if(age == 35){
+        println("You can buy alcohol")
+    }else{
+        println("Age not applicable")
+    }
+}
