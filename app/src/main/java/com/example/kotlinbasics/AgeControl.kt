48e0dbe83a77bf0261c321f667c729d556c1903d
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 16:42:07 +0800

    modify AgeControl if statement 'in' keyword

--- app/src/main/java/com/example/kotlinbasics/AgeControl.kt
+++ app/src/main/java/com/example/kotlinbasics/AgeControl.kt
@@ -8,7 +8,7 @@ fun main(){
     val enterAge = readln()
     age = enterAge.toInt()
 
-    if(age >= 18 && age < 40){
+    if(age in 18..39){
         println("You can enter the club.")
     }else if(age >= 40){
         println("You cannot go into the club, please go home.")
