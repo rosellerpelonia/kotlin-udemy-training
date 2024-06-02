e805d3bcb68877f2af0816500428acae0e43ea19
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 14:28:24 +0800

    kotlin basics day two

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    val constantNumber = 1
+    var mutableString = "mutable string"
+    println(mutableString)
+    mutableString = "mutable can be change"
+    println("$constantNumber $mutableString")
+
+
+}
