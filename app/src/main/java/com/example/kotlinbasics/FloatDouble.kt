bfcd427f18a93677d83788e2877a55c3e1755f70
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 14:56:29 +0800

    sample file for double and float

---
+++ app/src/main/java/com/example/kotlinbasics/FloatDouble.kt
@@ -0,0 +1,24 @@
+package com.example.kotlinbasics
+
+fun main(){
+    var e = 2.23f
+    println(e)
+    //round of in 7th number
+    e = 2.34342343455534f
+    println(e)
+
+    var f = 2.333334
+    println(f)
+    /*
+    * double is more precise than using float
+    * it doesn't round the number
+    * using var is mutable
+    * */
+
+    f = 2.23432432432242334324
+    println(f)
+
+
+
+
+}
