d3a75f7201b7b41cbbd2a236e0a98938f54826a4
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 15:40:39 +0800

    Strings

---
+++ app/src/main/java/com/example/kotlinbasics/Strings.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    var name = "Russ"
+    name = "very long paragraph can be stored inside of strings"
+
+    println(name.uppercase() + "concat")
+}
