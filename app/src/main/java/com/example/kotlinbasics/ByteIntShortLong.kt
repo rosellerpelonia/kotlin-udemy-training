38c40d248c9129a79d416643f414c824efde1362
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 14:41:47 +0800

    Int, Long, Short, Byte and specifying the type manually

---
+++ app/src/main/java/com/example/kotlinbasics/ByteIntShortLong.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasics
+
+fun main(){
+    //myAge data type is int
+    var myAge =31232
+    println(myAge)
+    //will cause error when declare the mutable as long
+    myAge = 21312312321
+    println(myAge)
+}
