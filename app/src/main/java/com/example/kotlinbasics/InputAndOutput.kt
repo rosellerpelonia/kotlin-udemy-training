e5edf8cabf2fc34131f6ea53b4fcd3529be6637c
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 16:21:22 +0800

    input and output

---
+++ app/src/main/java/com/example/kotlinbasics/InputAndOutput.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    var age = 0
+
+    println("Please enter you age: ")
+    val enteredValue = readln()
+    age = enteredValue.toInt()
+
+
+    if(age >= 18){
+        println("You can buy cigarettes")
+    }else{
+        println("You cannot buy cigarettes")
+    }
+
+}
