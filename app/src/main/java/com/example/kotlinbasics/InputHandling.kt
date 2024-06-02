7b5efdabe6a730cfb6c86620e0954ccc727a8cc8
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 18:16:26 +0800

    InputHandling

---
+++ app/src/main/java/com/example/kotlinbasics/InputHandling.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    println("Please enter a number:")
+    val inputString = readln()
+    val inputNumber = inputString.toInt()
+    val multiplier = 5
+    println(inputNumber * multiplier )
+
+}
