b6bfa7e0fe7093eeb88470565af007aa0cec87ab
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-02 17:29:38 +0800

    RockPaperScissorExercise

---
+++ app/src/main/java/com/example/kotlinbasics/RockPaperScissorExercise.kt
@@ -0,0 +1,48 @@
+package com.example.kotlinbasics
+
+fun main(){
+    var playAgain: Boolean
+    do {
+        var computerChoice = ""
+        var playerChoice = ""
+        println("Rock, Paper or Scissors? Enter your choice!")
+        playerChoice = readln()
+
+        val randomNumber = (1..3).random()
+        when (randomNumber) {
+            1 -> {
+                computerChoice = "Rock"
+            }
+
+            2 -> {
+                computerChoice = "Paper"
+            }
+
+            3 -> {
+                computerChoice = "Scissors"
+            }
+        }
+        println(computerChoice)
+
+        val winner = when {
+            playerChoice == computerChoice -> "Tie"
+            playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+            playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+            playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+            else -> "Computer"
+        }
+
+        if (winner == "Tie") {
+            println("Draw!")
+        } else if (winner == "Player") {
+            println("Player wins!")
+        } else {
+            println("Computer wins!")
+        }
+        println("Do you want to play again? (yes/no)")
+        val response = readln()
+        playAgain = response == "yes"
+    }while(playAgain)
+
+    println("Thanks for playing!")
+}
