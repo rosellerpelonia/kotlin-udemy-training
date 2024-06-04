4c793605b89897db7b80a8e73c2501258ac6d202
Roseller Pelonia <roseller.pelonia@gmail.com>
2024-06-04 20:33:28 +0800

    function with parameters

--- app/src/main/java/com/example/dayThreeKotlinBasics/FunctionsBasics.kt
+++ app/src/main/java/com/example/dayThreeKotlinBasics/FunctionsBasics.kt
@@ -3,15 +3,17 @@ package com.example.dayThreeKotlinBasics
 fun main(){
     println("Input sugar count")
     val sugarCount = readln().toInt()
-    makeCoffee(sugarCount)
+    println("Input the name of the coffee owner")
+    val name = readln()
+    makeCoffee(sugarCount, name)
 }
 
-fun makeCoffee(sugarCount : Int){
+fun makeCoffee(sugarCount : Int, name : String){
     if(sugarCount == 1){
-        println("Coffee with $sugarCount spoon of sugar")
+        println("Coffee with $sugarCount spoon of sugar for $name")
     }else if (sugarCount >= 1){
-        println("Coffee with $sugarCount spoons of sugar")
+        println("Coffee with $sugarCount spoons of sugar for $name")
     }else {
-        println("Coffee with no sugar")
+        println("Coffee with no sugar for $name")
     }
 }
